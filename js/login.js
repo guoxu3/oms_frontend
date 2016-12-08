b173bcdbeda0aef85658fcd6641bdfbf9c921b30
GuoXu <guoxu3@qq.com>
2016-12-08 10:39:38 +0800

    update20161208

---
+++ js/login.js
@@ -0,0 +1,40 @@
+/**
+ * Created by guoxu on 12/7/16.
+ */
+var login = function() {
+
+    var request = {};
+    for (var i = 0; i < arguments.length; i++) {
+        console.log("#"+arguments[i]);
+        request[arguments[i]] = $("#"+arguments[i]).val();
+    }
+
+    //调用了jquery.json 库
+    var encoded;
+    encoded = $.toJSON(request);
+    var jsonStr = encoded;
+    console.log(jsonStr);
+    var URL = 'http://api.oms.miaodeli.com/admin/user_login';
+    $.ajax({
+        url: URL,
+        type: 'POST',
+        data: jsonStr,
+        dataType: 'json',
+        contentType: 'application/json;charset=utf8',
+        success: function (data) {
+            // 解析收到的json数据
+            console.log(data);
+            var models = data;
+           // var models = $.parseJSON(data);
+            if (models.ok == true) {
+                //alert(models.info);
+                //window.location.href = "showtask.html";
+            } else {
+                alert(models.info);
+            }
+        },
+        Error: function (xhr, error, exception) {
+            alert(exception.toString());
+        }
+    });
+}
---
+++ js/task.js
@@ -0,0 +1,81 @@
+/**
+ * Created by guoxu on 12/7/16.
+ */
+function getTask() {
+    var URL = 'http://api.oms.miaodeli.com/api/task?start=0&count=10';
+    $.ajax({
+         type: "get",
+         url: URL,
+         success: function (data) {
+            // 解析收到的json数据
+            var models = $.parseJSON(data);
+            if (models.ok == true) {
+                $("#p1").html(data);
+            } else {
+                alert(models.info);
+            }
+         },
+         Error: function (xhr, error, exception) {
+            alert(exception.toString());
+         }
+    });
+}
+
+function addTask() {
+    var data = {};
+    for (var i = 0; i < arguments.length; i++) {
+        console.log("#"+arguments[i]);
+        data[arguments[i]] = $("#"+arguments[i]).val();
+    }
+    var request = {
+        action: 'add',
+        data: data
+    };
+    //调用了jquery.json 库
+    var encoded;
+    encoded = $.toJSON(request);
+    var jsonStr = encoded;
+    console.log(jsonStr);
+    var URL = 'http://api.oms.miaodeli.com/api/task';
+    $.ajax({
+        url: URL,
+        type: 'POST',
+        data: jsonStr,
+        dataType: 'json',
+        contentType: 'application/json;charset=utf8',
+        success: function (data) {
+            // 解析收到的json数据
+            var models = data;
+            if (models.ok == true) {
+                //alert(models.info);
+                window.location.href = "showtask.html";
+                //
+            } else {
+                alert(models.info);
+            }
+        },
+        Error: function (xhr, error, exception) {
+            alert(exception.toString());
+        }
+    });
+}
+
+function deleteTask(){
+    var URL = 'http://api.oms.miaodeli.com/api/task?start=0&count=10';
+    $.ajax({
+         type: "get",
+         url: URL,
+         success: function (data) {
+            // 解析收到的json数据
+            var models = $.parseJSON(data);
+            if (models.ok == true) {
+                $("#p1").html(data);
+            } else {
+                alert(models.info);
+            }
+         },
+         Error: function (xhr, error, exception) {
+            alert(exception.toString());
+         }
+    });
+}
