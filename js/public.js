b41eea2d4b20b968df5bf1143d1a226c21058f36
GuoXu <guoxu3@qq.com>
2017-02-15 11:14:58 +0800

    新增是否登陆逻辑判断，未登录自动跳转到登陆页面

--- js/public.js
+++ js/public.js
@@ -2,8 +2,7 @@
  * Created by guoxu on 2/14/17.
  */
 
-function getCookie(name)
-{
+function getCookie(name) {
     var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
     if(arr=document.cookie.match(reg))
         return unescape(arr[2]);
@@ -11,8 +10,32 @@ function getCookie(name)
         return null;
 }
 
-window.onload = function fillUsername(){
-    var user_name = getCookie("username");
-    document.getElementById("username").innerHTML = user_name;
+// 判断是否登陆或者是否登陆超时
+var username = getCookie("username");
+if (username == null) {
+        alert("Please log in first!");
+        window.location.href = "../login";
+} else {
+    var URL = 'http://oms.miaodeli.com/login';
+    $.ajax({
+         type: "GET",
+         url: URL,
+         success: function (data) {
+            // 解析收到的json数据
+            var models = $.parseJSON(data);
+            if (models.ok == false) {
+                alert("Please log in first!");
+                window.location.href = "../login";
+            }
+         },
+         Error: function (xhr, error, exception) {
+            alert(exception.toString());
+         }
+    });
+}
+
+// 自动填写html中的用户名
+window.onload = function fillUsername() {
+    document.getElementById("username").innerHTML = username;
 };
 
