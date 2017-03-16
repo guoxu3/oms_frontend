f9f7bb217607cd3ee6fb9e1b4cbf98f6f6a7d742
GuoXu <guoxu3@qq.com>
2017-03-16 17:53:01 +0800

    树状图

--- js/permission.js
+++ js/permission.js
@@ -12,7 +12,7 @@ function getAllPermission() {
         count = 10
     }
     var start = ((cur_page - 1) * count);
-    var URL = '/api/permission?start=' + start +'&count=' + count;
+    var URL = '/api/permission?start=' + start + '&count=' + count;
     var permission_data = [];
     var show_button = false;
     $.ajax({
@@ -34,7 +34,7 @@ function getAllPermission() {
                     show_button: show_button
                 }
             });
-            showPage(cur_page, Math.ceil(permission_num/count), count);
+            showPage(cur_page, Math.ceil(permission_num / count), count);
         },
         error: function (xhr, error, exception) {
             alert(exception.toString());
@@ -83,5 +83,64 @@ function addPermission() {
         error: function (xhr, error, exception) {
             alert(exception.toString());
         }
+    });
+}
+
+
+function showPermissionTree() {
+    var setting = {
+        check: {
+            enable: true
+        },
+        data: {
+            simpleData: {
+                enable: true
+            }
+        }
+    };
+
+    var zNodes = [
+        {id: 1, pId: 0, name: "随意勾选 1", open: true},
+        {id: 11, pId: 1, name: "随意勾选 1-1", open: true},
+        {id: 111, pId: 11, name: "随意勾选 1-1-1"},
+        {id: 112, pId: 11, name: "随意勾选 1-1-2"},
+        {id: 999, pId: 11, name: "随意勾选 19-19-2"},
+        {id: 12, pId: 1, name: "随意勾选 1-2", open: true},
+        {id: 121, pId: 12, name: "随意勾选 1-2-1"},
+        {id: 122, pId: 12, name: "随意勾选 1-2-2"},
+        {id: 2, pId: 0, name: "随意勾选 2", checked: true, open: true},
+        {id: 21, pId: 2, name: "随意勾选 2-1"},
+        {id: 22, pId: 2, name: "随意勾选 2-2", open: true},
+        {id: 221, pId: 22, name: "随意勾选 2-2-1", checked: true},
+        {id: 222, pId: 22, name: "随意勾选 2-2-2"},
+        {id: 23, pId: 2, name: "随意勾选 2-3"}
+    ];
+
+    var code;
+
+    function setCheck() {
+        var zTree = $.fn.zTree.getZTreeObj("treeDemo"),
+            py = $("#py").attr("checked") ? "p" : "",
+            sy = $("#sy").attr("checked") ? "s" : "",
+            pn = $("#pn").attr("checked") ? "p" : "",
+            sn = $("#sn").attr("checked") ? "s" : "",
+            type = {"Y": py + sy, "N": pn + sn};
+        zTree.setting.check.chkboxType = type;
+        showCode('setting.check.chkboxType = { "Y" : "' + type.Y + '", "N" : "' + type.N + '" };');
+    }
+
+    function showCode(str) {
+        if (!code) code = $("#code");
+        code.empty();
+        code.append("<li>" + str + "</li>");
+    }
+
+    $(document).ready(function () {
+        $.fn.zTree.init($("#treeDemo"), setting, zNodes);
+        setCheck();
+        $("#py").bind("change", setCheck);
+        $("#sy").bind("change", setCheck);
+        $("#pn").bind("change", setCheck);
+        $("#sn").bind("change", setCheck);
     });
 }
