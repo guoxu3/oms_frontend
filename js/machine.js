c52adfc8ed7b038f2bb55f6b3e956823182ffffc
guoxu3 <guoxu3@qq.com>
2017-04-10 21:45:55 +0800

    update

--- js/machine.js
+++ js/machine.js
@@ -22,8 +22,15 @@ $(function () {
             initialized_options: [
                 { text: '是', value: true},
                 { text: '否', value: false}
-            ]
-        },
+            ],
+            nginx: '',
+            mysql: '',
+            php: '',
+            redis: '',
+            memcache: '',
+            jdk: '',
+            tomcat: ''
+        }
     });
 
     window.machinelist = new Vue({
