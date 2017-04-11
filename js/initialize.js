133e72b3e177888d0f81c8cfa6b27e99ec815104
guoxu3 <guoxu3@qq.com>
2017-04-11 22:55:51 +0800

    初始化相关js

---
+++ js/initialize.js
@@ -0,0 +1,77 @@
+/**
+ * Created by guoxu on 4/11/17.
+ */
+
+function machine_initialize() {
+    var ip = GetQueryString('ip');
+    var request = {
+        action: 'initialize',
+        data: {
+            'ip': ip
+        }
+    };
+    var encoded;
+    encoded = $.toJSON(request);
+    var jsonStr = encoded;
+    var URL = '/api/initialize';
+    $.ajax({
+        url: URL,
+        type: 'POST',
+        data: jsonStr,
+        dataType: 'json',
+        contentType: 'application/json;charset=utf8',
+        success: function (data) {
+            var models = data;
+            if (models.ok === true) {
+                alert(models.info)
+            } else {
+                alert(models.info);
+            }
+        },
+        error: function (xhr, error, exception) {
+            alert(exception.toString());
+        }
+    });
+}
+
+
+function install_software() {
+    var ip = GetQueryString('ip');
+    var obj = document.getElementsByName("software");
+    var software_list = [];
+    var k;
+    for (k in obj) {
+        if (obj[k].checked)
+            software_list.push(obj[k].value);
+    }
+
+    var request = {
+        action: 'install',
+        data: {
+            'ip': ip,
+            'software': software_list
+        }
+    };
+    var encoded;
+    encoded = $.toJSON(request);
+    var jsonStr = encoded;
+    var URL = '/api/task';
+    $.ajax({
+        url: URL,
+        type: 'POST',
+        data: jsonStr,
+        dataType: 'json',
+        contentType: 'application/json;charset=utf8',
+        success: function (data) {
+            var models = data;
+            if (models.ok === true) {
+                window.location.href = "/task/list";
+            } else {
+                alert(models.info);
+            }
+        },
+        error: function (xhr, error, exception) {
+            alert(exception.toString());
+        }
+    });
+}
